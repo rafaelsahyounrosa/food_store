06cee1ad20e07bc416571f58efb1fa2877ce2970
Rafael Sahyoun Rosa <83467259+rafaelsahyounrosa@users.noreply.github.com>
2024-04-19 20:43:28 -0300

    Link estatico

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -67,7 +67,7 @@ const Restaurant = ({
             </RateDiv>
           </TitleContainer>
           <Description>{getDescription(descricao)}</Description>
-          <Link to={`/restaurant/${id}`}>
+          <Link to={`/restaurant/1`}>
             {/* Passar solução definitiva para to={`/restaurant/${id}`}> */}{' '}
             <Button>Saiba mais</Button>{' '}
           </Link>
